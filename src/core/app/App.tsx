0467bcd8a3591d656901fd35424a53a1c80533d1
Covadonga M <cova.millan@gmail.com>
2023-02-09 20:29:09 +0100

    Divide html file in components

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Actions } from "../../features/actions/actions";
+import { Info } from "../../features/info/info";
+import { Key } from "../../features/key/key";
+import { Keyboard } from "../../features/keyboard/keyboard";
 
 function App() {
-  return <App></App>;
+  return (
+    <>
+      <Info></Info>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/features/actions/actions.tsx
@@ -0,0 +1,13 @@
+import { Action } from "../action/action";
+import { Display } from "../display/diplay";
+
+export function Actions() {
+  return (
+    <>
+      <div className="actions">
+        <Display></Display>
+        <Action></Action>
+      </div>
+    </>
+  );
+}
